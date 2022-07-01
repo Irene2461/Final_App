23af54c68984041398f55fdf5b8d23fcbc2799bb
Irene2461 <ngochoang240601@gmail.com>
2022-07-01 10:20:38 +0700

    First migration update

---
+++ db/migrate/20220701031352_create_albums.rb
@@ -0,0 +1,12 @@
+class CreateAlbums < ActiveRecord::Migration[7.0]
+  def change
+    create_table :albums do |t|
+      t.string :title
+      t.string :desc
+      t.string :src
+      t.boolean :is_private
+      t.references :user, foreign_key: true, index: true
+      t.timestamps
+    end
+  end
+end
