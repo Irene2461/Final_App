46195fe6073f6538051599939af6cfb26cb73b51
Irene2461 <ngochoang240601@gmail.com>
2022-07-05 13:57:20 +0700

    Fix PR #2 in Association

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,5 @@
 class Album < ApplicationRecord
     belongs_to :user
-    has_many :photos
-    has_many :likes, as: :likeable, dependent: :destroy
+    has_many :photos, dependent: :destroy
+    has_many :likes, as: :likeable, dependent: :nullify
 end
